1f9ac1fd4125b4c8774443068f3fec7b1764b8f4
Scott Mallinson <scott@scottmallinson.com>
2019-04-02 19:32:27 +0200

    Iteration 2: Conditionals completed

--- basic-algorithms.js
+++ basic-algorithms.js
@@ -7,5 +7,20 @@ console.log("The navigator's name is "+hacker2); // Print "The navigator's name 
 
 //Conditionals
 
+/* 
+Depending on which name is longer, print:
+  The Driver has the longest name, it has XX characters or
+  Yo, navigator got the longest name, it has XX characters or
+  wow, you both got equally long names, XX characters!!
+*/
+if (hacker1.length > hacker2.length) {
+  console.log("The Driver has the longest name, it has "+hacker1.length+" characters");
+}
+else if (hacker1.length < hacker2.length) {
+  console.log("Yo, navigator got the longest name, it has "+hacker2.length+" characters");
+}
+else {
+  console.log("wow, you both got equally long names, "+hacker1.length+" characters!!")
+}
 
 // Lorem ipsum generator
