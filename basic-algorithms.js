27f03d8a48f0d242397320859c4c9910626a4f60
Jesus Cebader <zebader@hotmail.com>
2019-04-02 18:13:21 +0200

    iteration 1

--- basic-algorithms.js
+++ basic-algorithms.js
@@ -1,6 +1,8 @@
 // Names and Input
-
-
+var hacker1 = "CrazyDriver";
+console.log("The driver's name is"+hacker1);
+var hacker2 = prompt("What's the navigator name?");
+console.log("The navigator's name is"+hacker2);
 //Conditionals
 
 
