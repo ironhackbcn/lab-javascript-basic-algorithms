027724a9a8f0b0f56281769bcba7543ec97ffea7
Scott Mallinson <scott@scottmallinson.com>
2019-04-02 21:00:13 +0200

    Iteration 3: Loops and Palidrome completed

--- basic-algorithms.js
+++ basic-algorithms.js
@@ -23,4 +23,69 @@ else {
   console.log("wow, you both got equally long names, "+hacker1.length+" characters!!")
 }
 
-// Lorem ipsum generator
+// Loops
+
+// Print all the characters of the driver's name, separated by a space and in capitals ie. "J O H N"
+
+var driversName = "";
+
+for(i = 0; i < hacker1.length; i++) {
+  driversName += hacker1[i] + ' ';
+}
+console.log(driversName.toUpperCase());
+
+// Print all the characters of the navigator's name, in reverse order. ie. "nhoJ"
+
+var navigatorsName = "";
+
+for(var i = hacker2.length-1; i >= 0; i-- ) {
+  navigatorsName += hacker2[i];
+}
+console.log(navigatorsName);
+
+/*
+Depending on the lexicographic order of the strings, print:
+  The driver's name goes first
+  Yo, the navigator goes first definitely
+  What?! You both got the same name?
+*/
+
+var hacker1 = "Dave";
+var hacker2 = "Timothy";
+
+if (hacker1.localeCompare(hacker2) < 0) {
+  console.log("The driver's name goes first");
+}
+else if (hacker1.localeCompare(hacker2) > 0) {
+  console.log("Yo, the navigator goes first definitely");
+}
+else {
+  console.log("What?! You both got the same name?");
+}
+
+/*
+Ask the user for a new string and check if it's a Palindrome. Examples of palindromes:
+
+"A man, a plan, a canal, Panama!"
+"Amor, Roma"
+"race car"
+"stack cats"
+"step on no pets"
+"taco cat"
+"put it up"
+"Was it a car or a cat I saw?" and "No 'x' in Nixon".
+*/
+
+var newString = prompt("Enter a word or phrase:").replace(/[^a-zA-Z0-9]/g,'');
+var reversedString = "";
+
+for(var i = newString.length-1; i >= 0; i-- ) {
+  reversedString += newString[i].toLowerCase();
+}
+
+if (reversedString == newString.toLowerCase()) {
+  console.log("Palindrome!");
+}
+else {
+  console.log("404 Palindrome not found");
+}
