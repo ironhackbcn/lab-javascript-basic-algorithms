f0b7bbdc0f42e0d0cb02ac74a940167bdf7978ed
Scott Mallinson <scott@scottmallinson.com>
2019-04-02 19:27:32 +0200

    Iteration 1: Names and Input completed

--- basic-algorithms.js
+++ basic-algorithms.js
@@ -1,5 +1,9 @@
 // Names and Input
 
+var hacker1 = "Dave"; // Create a variable hacker1 with the driver's name
+console.log("The driver's name is "+hacker1); // Print "The driver's name is XXXX"
+var hacker2 = prompt("Enter your name:"); //Create a variable hacker2 and ask the user for the navigator's name
+console.log("The navigator's name is "+hacker2); // Print "The navigator's name is YYYY"
 
 //Conditionals
 
