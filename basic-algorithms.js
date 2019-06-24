9f7c1d590c684ac3e6a039ea77949a859cba01ff
Gerard Montane <gmontanee@gmail.com>
2019-06-25 00:16:11 +0200

    done

--- basic-algorithms.js
+++ basic-algorithms.js
@@ -1,7 +1,22 @@
 // Names and Input
+var hacker1 = 'Gerard';
+console.log("The driver's name is ", hacker1);
 
+var hacker2;
+document.write(hacker2);
+console.log("The navegator's name is ", hacker2);
 
 //Conditionals
-
-
+if (String.length(hacker1) > String.length(hacker2)) {
+  console.log("The Driver has the longest name, it has", String.length(hacker1), "characters or");
+}
+else if (String.length(hacker1) < String.length(hacker2)) {
+  console.log("Yo, navigator got the longest name, it has ", String.length(hacker2), " characters");
+}
+else {
+  console.log("wow, you both got equally long names, ", String.length(hacker1), " characters!!")
+}
 // Lorem ipsum generator
+for (var i = 0; i < toString.length(hacker1); ++i) {
+console.log(hacker1[i].toUpperCase);
+}
