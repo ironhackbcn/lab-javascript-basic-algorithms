5c3bd126740505544671de9f362dc0d9d6b2869d
Rafael Bosch <rbllado@outlook.com>
2019-10-21 22:35:18 +0200

    Done it until Conditional

--- basic-algorithms.js
+++ basic-algorithms.js
@@ -1,7 +1,23 @@
 // Names and Input
+var hacker1 = "alonso";
+var hacker2 = "";
+
+hacker2 = prompt();
+
+console.log("The driver's name is " + hacker1);
+console.log("The navigator´s name is " + hacker2);
 
 
 //Conditionals
+
+if(hacker1.length > hacker2.length){
+    console.log("The Driver has the longest name, it has "+ hacker1.length +" characters");
+}else if(hacker1.length < hacker2.length){
+    console.log("The Navigator got the longest name, it has "+ hacker2.length +" characters");
+}else{
+    console.log("wow, you both got equally long names," + hacker2.length +" characters!!");
+    
+}
 
 
 // Lorem ipsum generator
